f70a85ac861c9673f1043cc974b1029be6b65a08
SUmarFarooq <umarfarooq95@gmail.com>
2016-12-31 16:36:49 -0600

    Add files via upload

--- textid_final.py
+++ textid_final.py
@@ -1,4 +1,4 @@
-#JennyY(Yuqing) Cang, Grace Stewart, and Umar Farooq
+#Jenny(Yuqing) Cang, Grace Stewart, and Umar Farooq
 #Final woo!
 
 
@@ -258,26 +258,148 @@ class TextModel:
 		self.makePunctuation(s)
 		self.makeWordLengths(new_s)
 
-print(" +++++++++++ Model1 +++++++++++ ")
-trained_tm1 = TextModel( "Model1" )
-text1 = trained_tm1.readTextFromFile( "train1.txt" )
-trained_tm1.createAllDictionaries(text1)  # provided in hw description
-trained_tm1.printAllDictionaries()
 
-print(" +++++++++++ Model2 +++++++++++ ")
-trained_tm2 = TextModel( "Model2" )
-text2 = trained_tm2.readTextFromFile( "train2.txt" )
-trained_tm2.createAllDictionaries(text2)  # provided in hw description
-trained_tm2.printAllDictionaries()
-
-print(" +++++++++++ Unknown text +++++++++++ ")
-unknown_tm = TextModel( "Unknown (trial)" )
-text_unk = unknown_tm.readTextFromFile( "unknown.txt" )
-unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
-unknown_tm.printAllDictionaries()
-
-
-	# def compareTextWithTwoModels(self,model1,model2):
-	# 	""" runs the compareDictionaries method for all feature dictionaries in self against corresponding normalized dictionaries in model1 and model2
-	# 	"""
+	def compareTextWithTwoModels(self,model1,model2):
+		""" runs the compareDictionaries method for all feature dictionaries in self against corresponding normalized dictionaries in model1 and model2
+		"""
 	
+		a = self.normalizeDictionary(model1.words)
+		b = self.normalizeDictionary(model2.words)
+		L1 = self.compareDictionaries(self.words,a,b)
+		
+		c = self.normalizeDictionary(model1.wordlengths)
+		d = self.normalizeDictionary(model2.wordlengths)
+		L2 = self.compareDictionaries(self.wordlengths,c,d)
+
+		e = self.normalizeDictionary(model1.sentencelengths)
+		f = self.normalizeDictionary(model2.sentencelengths)
+		L3 = self.compareDictionaries(self.sentencelengths,e,f)
+
+		g = self.normalizeDictionary(model1.stems)
+		h = self.normalizeDictionary(model2.stems)
+		L4 = self.compareDictionaries(self.stems,g,h)
+
+		i = self.normalizeDictionary(model1.punc)
+		j = self.normalizeDictionary(model2.punc)
+		L5 = self.compareDictionaries(self.punc,i,j)
+
+		print("Overall comparison of",self.name, "vs", model1.name,'and',model2.name,': \n')
+
+		print("{0:15} {1:<15} {2:<15}".format("name","vsTM1","vsTM2"))
+		print("{0:15} {1:<15} {2:<15}".format("----","-----","-----"))
+		print("{0:15} {1:<15.2f} {2:<15.2f}".format("words",L1[0],L1[1]))
+		print("{0:15} {1:<15.2f} {2:<15.2f}".format("wordlengths",L2[0],L2[1]))
+		print("{0:10} {1:<15.2f} {2:<15.2f}".format("sentencelengths",L3[0],L3[1]))
+		print("{0:15} {1:<15.2f} {2:<15.2f}".format("stems",L4[0],L4[1]))
+		print("{0:15} {1:<15.2f} {2:<15.2f}".format("punctuation",L5[0],L5[1]))
+
+		model1wins = 0
+		model2wins = 0
+
+		if L1[0] > L1[1]:
+			model1wins += 1
+		else: 
+			model2wins +=1
+
+		if L2[0] > L2[1]:
+			model1wins += 1
+		else: 
+			model2wins +=1		
+		
+		if L3[0] > L3[1]:
+			model1wins += 1
+		else: 
+			model2wins +=1		
+		
+		if L4[0] > L4[1]:
+			model1wins += 1
+		else: 
+			model2wins +=1
+
+		if L5[0] > L5[1]:
+			model1wins += 1
+		else: 
+			model2wins +=1
+		
+		print("\nModel1 - ",model1.name, "wins on", model1wins," features")
+		print("Model2 - ",model2.name, "wins on", model2wins," features")
+		if model1wins > model2wins:
+			print("\n +++++      Model1 -", model1.name, "is the better match!      +++++\n")
+		else:
+			print("\n +++++      Model2 -", model2.name, "is the better match!      +++++\n")
+
+
+print(" +++++++++++ Friends +++++++++++ ")
+trained_tm1 = TextModel( "Friends" )
+text1 = trained_tm1.readTextFromFile( "Friends.txt" )
+trained_tm1.createAllDictionaries(text1)  # provided in hw description
+# trained_tm1.printAllDictionaries()
+
+print(" +++++++++++ Seinfeld +++++++++++ ")
+trained_tm2 = TextModel( "Seinfeld" )
+text2 = trained_tm2.readTextFromFile( "Seinfeld.txt" )
+trained_tm2.createAllDictionaries(text2)  # provided in hw description
+# trained_tm2.printAllDictionaries()
+
+print(" +++++++++++ Test1 +++++++++++ ")
+unknown_tm = TextModel( "Test1" )
+text_unk = unknown_tm.readTextFromFile( "Test1.txt" )
+unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# # unknown_tm.printAllDictionaries()
+
+# print(" +++++++++++ Test2 +++++++++++ ")
+# unknown_tm = TextModel( "Test2" )
+# text_unk = unknown_tm.readTextFromFile( "Test2.txt" )
+# unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# #unknown_tm.printAllDictionaries()
+
+# print(" +++++++++++ Test3 +++++++++++ ")
+# unknown_tm = TextModel( "Test3" )
+# text_unk = unknown_tm.readTextFromFile( "Test3.txt" )
+# unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# #unknown_tm.printAllDictionaries()
+
+unknown_tm.compareTextWithTwoModels(trained_tm1,trained_tm2)
+
+""" RESULTS
+Our two models were based on scripts from Friends and Seinfeld, testing whether or not a random episode's script was more likely to be from Seinfeld or from Friends. Our model is relatively straightforward: whichever model has the most dictionaries match the text, that model wins. 
+
+We found that our models mostly worked. Initially, they didn't work very well at all. We chose an episode from Friends and one from Seinfeld as our corpuses (corpi?) as well as an episode from each as our unknown. Each result was the opposite of the real answer (e.g., our model would find that the test episode from Friends was more likely to be from Seinfeld than from Friends.) Prof Dodds told us to test on half an episode instead of a whole one, and this fixed our problem. We then expanded our corpuses to 4 episodes per show and tested on the script from a whole episode instead of a half. We found that our model returned the correct results for the test script from Friends (3-2 in favor of Friends) but still did not return the right result for the Seinfeld test episode.
+
+We did not quite expect these results. Straight up, we thought these would be pretty straightforward wins for one show over the other, especially because of the presence of character names in each show. Still, we think it's possible the models are so close to one another because of the general, everyday language used in sitcoms, and because the scripts contain things like stage cues (Jerry Seinfeld sitting on couch, Kramer walks in) and things like that. """ 
+
+# EXTRA (CREDIT) Text Models: Musicals
+
+print(" +++++++++++ Sondheim Musicals +++++++++++ ")
+trained_tm1 = TextModel( "Sondheim" )
+text1 = trained_tm1.readTextFromFile( "Sondheim.txt" )
+trained_tm1.createAllDictionaries(text1)  # provided in hw description
+# trained_tm1.printAllDictionaries()
+
+print(" +++++++++++ Schwartz Musicals +++++++++++ ")
+trained_tm2 = TextModel( "Schwartz" )
+text2 = trained_tm2.readTextFromFile( "Schwartz.txt" )
+trained_tm2.createAllDictionaries(text2)  # provided in hw description
+# trained_tm2.printAllDictionaries()
+
+print(" +++++++++++ 1st Test: Wicked +++++++++++ ")
+unknown_tm = TextModel( "Test (Wicked)" )
+text_unk = unknown_tm.readTextFromFile( "Wicked.txt" )
+unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# unknown_tm.printAllDictionaries()
+
+# print(" +++++++++++ 2nd Test: Into the Woods +++++++++++ ")
+# unknown_tm = TextModel( "Test (Into the Woods)" )
+# text_unk = unknown_tm.readTextFromFile( "IntotheWoods.txt" )
+# unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# # unknown_tm.printAllDictionaries()
+
+# print(" +++++++++++ 3rd Test: West Side Story +++++++++++ ")
+# unknown_tm = TextModel( "Test (West Side Story)" )
+# text_unk = unknown_tm.readTextFromFile( "WestSideStory.txt" )
+# unknown_tm.createAllDictionaries(text_unk)  # provided in hw description
+# # unknown_tm.printAllDictionaries()
+
+unknown_tm.compareTextWithTwoModels(trained_tm1,trained_tm2)
+
+""" For musicals, I created a model of musicals by Stephen Sondheim (Into the Woods and Sweeney Todd), and a model of musicals by Stephen Schwartz (Wicked and Pippin). For my tests, the first 2 returned the correct result, which makes sense because they are excerpts of the musicals scripts in the models. However, for a third test, I wanted to see if a different musical by Sondheim, West Side Story, would return the correct result, and it didn't work. I think for musicals, there is a lot of variability in storyline and song meter, so something like sentence lengths, words, and punctuation can't capture who wrote it. """
